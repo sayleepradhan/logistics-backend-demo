0b3235eb8a576cc8362b613ae37f7b4e78ce048b
sayleepradhan <saylee.pradhan@gmail.com>
2026-04-02 22:15:32 -0400

    chore: refactor router.py code to a separate service layer

---
+++ app/service/shipment.py
@@ -0,0 +1,51 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from datetime import datetime, timedelta
+from fastapi import HTTPException, status
+
+from app.schemas.shipment import Shipment, ShipmentCreate, ShipmentUpdate
+from app.database.models import ShipmentStatus
+
+
+class ShipmentService:
+    def __init__(self, session: AsyncSession):
+        self.session = session
+
+    async def get(self, id: int) -> Shipment:
+        return await self.session.get(Shipment, id)
+
+    async def add(self, shipment_create: ShipmentCreate) -> Shipment:
+        new_shipment = Shipment(
+            **shipment_create.model_dump(),
+            status = ShipmentStatus.placed,
+            estimated_delivery = datetime.now() + timedelta(days=3)
+        )
+        self.session.add(new_shipment)
+        await self.session.commit()
+        await self.session.refresh(new_shipment)
+
+        return new_shipment
+
+    async def update(self, shipment_update: ShipmentUpdate) -> Shipment:
+        shipment = self.get(id)
+        if shipment is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail = "Given id doesn't exist"
+                )
+        if not shipment_update:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail = "No data provided to update"
+            )
+        shipment.sqlmodel_update(shipment_update)
+        self.session.add(shipment)
+        await self.session.commit()
+        await self.session.refresh(shipment)
+
+        return shipment
+
+    async def delete(self, id: int) -> None:
+        self.session.delete(
+            self.get(id)
+        )
+        return await self.session.commit()
