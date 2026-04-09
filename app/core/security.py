4be39e73f1a3956576867e7f47571f824b075350
sayleepradhan <saylee.pradhan@gmail.com>
2026-04-08 23:51:11 -0400

    Add AccessTokenBearer class

--- app/core/security.py
+++ app/core/security.py
@@ -1,3 +1,27 @@
-from fastapi.security import OAuth2PasswordBearer
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, OAuth2PasswordBearer
+
+from app.services.utils import decode_access_token
 
 oauth2_scheme =OAuth2PasswordBearer("/seller/token")
+
+class AccessTokenBearer(HTTPBearer):
+    async def __call__(self, request):
+        auth_credentials = await super().__call__(request)
+        token = auth_credentials.credentials
+
+        token_data = decode_access_token(token)
+
+        if token_data is None:
+            raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Not authorized"
+        )
+
+        return token_data
+
+access_token_bearer = AccessTokenBearer()
+
+Annotated[dict, Depends(access_token_bearer)]
